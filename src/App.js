fbadec1286bd8645007e237a52bc836b02fed426
rifaturrana <smrifaturrana@gmail.com>
2022-06-11 00:57:00 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-
-import './App.css';
-import Login from './Pages/Login/Login';
-import Home from './Pages/Homepage/Home';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import "./App.css";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Homepage/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    
-    <div className="App">
-      
-   <Router>
-    <Switch>
-     <Route exact path='/'>
-      <Home></Home>
-     </Route>
-     <Route exact path='/login'>
-      <Login></Login>
-
-     </Route>
-
-      
-    </Switch>
-   </Router>
-    
-      
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,16 +1,14 @@
 import React from "react";
-//import '../../App.css';
-import './home.css';
+
+import "./home.css";
 import Navbar from "../Navbar/Navbar";
 
-const Home=()=>{
-
-    return(
-        <div className="main">
-<Navbar></Navbar>
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
     </div>
-    );
-    
-}
+  );
+};
 
 export default Home;
